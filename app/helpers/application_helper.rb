22999a252da863dc16ad8e2d00637e358c5beab5
Josh Nichols <josh@technicalpickles.com>
2009-04-26 12:47:48 -0400

    Moving towards better colored graphs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,12 @@ module ApplicationHelper
   end
 
   def google_pie_chart(data, options = {})
+  end
+
+
+  def result_chart(battle, options = {})
+    data = [ [battle.battle_matchups.left.the_random.to_s, battle.battle_matchups.left.votes.count], [battle.battle_matchups.right.the_random.to_s, battle.battle_matchups.right.votes.count]]
+
     options[:width] ||= 250
     options[:height] ||= 100
     options[:colors] = %w(0DB2AC F5DD7E FC8D4D FC694D FABA32 704948 968144 C08FBC ADD97E)
@@ -23,12 +29,7 @@ module ApplicationHelper
       :chco => options[:colors].slice(0, data.length).join(',')
     }
 
-    image_tag("http://chart.apis.google.com/chart?#{opts.map{|k,v|"#{k}=#{v}"}.join('&')}")
-  end
-
-
-  def result_chart(battle)
-    chart = google_pie_chart [ [battle.battle_matchups.left.the_random.to_s, battle.battle_matchups.left.votes.count], [battle.battle_matchups.right.the_random.to_s, battle.battle_matchups.right.votes.count]]
+    chart = image_tag("http://chart.apis.google.com/chart?#{opts.map{|k,v|"#{k}=#{v}"}.join('&')}")
 
     link_to chart, result_path(battle)
   end
