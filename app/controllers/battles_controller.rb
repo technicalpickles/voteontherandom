c09d9eb6c2181090fe916318f40d40bd986ee59d
Josh Nichols <josh@technicalpickles.com>
2009-04-26 10:05:26 -0400

    Remember the last voted thing.

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,12 +1,15 @@
 class BattlesController < ApplicationController
   def index
     @battle = Battle.next_to_vote_on_by(request.session_options[:id])
+    @last_battle = Battle.find(flash[:last_id]) if flash[:last_id]
+
   end
 
   def vote
     @battle = Battle.find params[:id]
-    debugger
     @battle.vote_on_side!(params[:side], request.session_options[:id])
+
+    flash[:last_id] = @battle.to_param
 
     redirect_to root_url
   end
