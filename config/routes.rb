18784894d05ff5e3b1f05e7f2c5a66cb006cd965
Josh Nichols <josh@technicalpickles.com>
2009-04-26 10:54:47 -0400

    Added a wtf page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :battles, :member => { :vote => :post }
   map.resources :results
 
+  map.wtf '/wtf', :controller => 'pages', :action => 'wtf'
   map.root :battles
 end
